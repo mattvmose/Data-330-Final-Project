d4ddfab1cb4c1f23c4028aa586388b102dbaced9
mattvmose <mattvmose@gmail.com>
2025-12-01 12:45:38 -0500

    Replace Amazon data with Safeway data processing

--- clean_price_data.R
+++ clean_price_data.R
@@ -5,13 +5,13 @@ library(dplyr)
 library(stringr)
 
 # Loading raw CSVs into R
-amazon_raw <- read.csv("raw_price_data/raw_amazon_data.csv")
+safeway_raw <- read.csv("raw_price_data/raw_safeway_data.csv")
 target_raw <- read.csv("raw_price_data/raw_target_data.csv")
 
 # Normalizing Data Types
-amazon_raw <- amazon_raw |> mutate(Product_Price = as.numeric(gsub("[^0-9\\.]", "", Product_Price)),
+safeway_raw <- safeway_raw |> mutate(Product_Price = as.numeric(gsub("[^0-9\\.]", "", Product_Price)),
                                      Date = as.Date(Date, format="%Y-%m-%d"),
-                                     Product_Name = str_remove(Product_Name, ",.*"))
+                                     Detail = c("Eggs", "Milk", "Sandwich Bread", "Sandwich Cheese", "Citrus_Lemon", "Citrus_Orange", "Apples", "Tomatoes", "Lettuce & Greens"))
 
 target_raw <- target_raw |> mutate(Product_Price = as.numeric(Product_Price),
                                    Date = as.Date(Date, format="%Y-%m-%d"),
@@ -20,25 +20,25 @@ target_raw <- target_raw |> mutate(Product_Price = as.numeric(Product_Price),
 
 # Creating lookup table to map walmart_raw products to target_raw products
 lookup <- tibble(
-  amazon_raw_names = c("Vital Farms", "Horizon Organic High Vitamin D Whole Milk", "Nature's Own Honey Wheat", "Kraft Singles American Slices", "Lemon", "Navel Orange", "Gala Apple", "Organic Roma Tomato", "Iceberg Lettuce"),
+  safeway_raw_names = c("Eggs", "Milk", "Sandwich Bread", "Sandwich Cheese", "Citrus_Lemon", "Citrus_Orange", "Apples", "Tomatoes", "Lettuce & Greens"),
   target_raw_names = c("Vital Farms Pasture Raised Eggs", "Horizon Organic High Vitamin D Milk", "Nature's Own Honey Wheat Bread", "Kraft Singles American Cheese Slices", "Fresh Lemon", "Fresh Navel Orange", "Fresh Gala Apple", "Fresh Organic Roma Tomatoes", "Fresh Iceberg Lettuce Head")
 )
 
 # Applying lookup to target_raw
-amazon_raw_clean <- amazon_raw |>
-  left_join(lookup, by=c("Product_Name" = "amazon_raw_names")) |>
+safeway_raw_clean <- safeway_raw |>
+  left_join(lookup, by=c("Detail" = "safeway_raw_names")) |>
   mutate(Product_Name = coalesce(target_raw_names, Product_Name))
 
 # Joining Walmart and Target Data
-clean_data <- full_join(amazon_raw_clean, target_raw, by=c("Date", "Product_Name"))
+clean_data <- full_join(safeway_raw_clean, target_raw, by=c("Date", "Product_Name"))
 
 clean_data <- clean_data |>
   rename(
     Date = Date,
-    Amazon_Price = Product_Price.x,
+    Safeway_Price = Product_Price.x,
     Target_Price = Product_Price.y,
   ) |>
-  select(Date, Product_Name, Amazon_Price, Target_Price)
+  select(Date, Product_Name, Safeway_Price, Target_Price)
 
 # Writing to CSV
 output_file <- "clean_price_data/price_data.csv"
