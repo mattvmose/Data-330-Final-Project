075992db014e7f1f748b0ec8620491eda7e130c7
mattvmose <mattvmose@gmail.com>
2025-12-01 21:50:09 -0500

    Enhance scrapeCOL.R with product names and CSV output
    Added product names and saved data to CSV.

--- scrapeCOL.R
+++ scrapeCOL.R
@@ -11,10 +11,11 @@ page <- read_html(url)
 prices <- page |> html_nodes("span.first_currency") |> html_text()
 pattern <- c(12, 9, 10, 13, 17, 18, 16, 19, 22)
 prices <- prices[pattern]
+product_names <- c("Vital Farms Pasture Raised Eggs", "Horizon Organic High Vitamin D Milk", "Nature&#39;s Own Honey Wheat Bread - 20oz,3.32", "Kraft Singles American Cheese Slices - 16oz/24ct", "Fresh Banana - each - Good &#38; Gather&#8482;", "Fresh Navel Orange - each", "Fresh Gala Apple - each", "Fresh Organic Roma Tomatoes - 1lb", "Fresh Iceberg Lettuce Head - each")
 
 COL_data <- tibble(
   Date = Sys.Date(),
-  Product_Name = names,
+  Product_Name = product_names,
   Product_Price = prices
 )
 
